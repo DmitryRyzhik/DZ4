ae15e50485ff7e9d0e3748bb2873f37b9feb46aa
DmitryRyzhik <rdssdr03@gmail.com>
2023-03-02 22:10:43 +0800

    Решение задачи №2

---
+++ 2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadLine(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Count(int number)
+
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+
+int number = ReadLine("Введите число: ");
+
+int sum = Count (number);
+
+System.Console.Write(sum);
