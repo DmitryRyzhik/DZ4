a75d65e4c6c8b8eb7b4761a2771346452866644c
DmitryRyzhik <rdssdr03@gmail.com>
2023-03-02 19:17:18 +0800

    Решение здачи №1

---
+++ 1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int ReadLine(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = ReadLine("Введите число А: ");
+
+int number1 = ReadLine("Введите число А: ");
+
+System.Console.Write(Math.Pow(number, number1));
